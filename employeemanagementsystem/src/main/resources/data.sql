afcda1edb483fa756daf85dc02d4fe64f998cec6
Meenakshi Sharma <meenasharma282003@gmail.com>
2024-08-30 16:37:00 +0530

    employee management system

--- employeemanagementsystem/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO departments (name) VALUES ('HR');
-INSERT INTO departments (name) VALUES ('IT');
-
-INSERT INTO employees (name, email, department_id) VALUES ('John Doe', 'john.doe@example.com', 1);
-INSERT INTO employees (name, email, department_id) VALUES ('Jane Smith', 'jane.smith@example.com', 2);
